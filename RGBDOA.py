9e202a2a7a4843f0f137ffe0d56406685c6ae369
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2019-05-03 11:31:12 +0200

    Update RGBDOA.py
    Improved version with direct point cloud calculation (D435 ASIC)

--- RGBDOA.py
+++ RGBDOA.py
@@ -5,19 +5,26 @@ import numpy as np
 import cv2
 import imutils
 import pyrealsense2 as rs
-from open3d import *
 from matplotlib import pyplot
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import time
 
 # Camera intrinsic parameters
-fx = 641.66
-fy = 641.66
-cx = 324.87
-cy = 237.38
+fx = 384.996
+fy = 384.996
+cx = 325.85
+cy = 237.646
 
+# width: 640, height: 480, ppx: 325.85, ppy: 237.646, fx: 384.996, fy: 384.996, model: Brown Conrady, coeffs: [0, 0, 0, 0, 0]
 MIP = np.array([[fx,0,cx],[0,fy,cy],[0,0,1]])
+
+# Declare pointcloud object, for calculating pointclouds and texture mappings
+pc = rs.pointcloud()
+decimate = rs.decimation_filter()
+decimate.set_option(rs.option.filter_magnitude, 2**3)
+# We want the points object to be persistent so we can display the last cloud when a frame drops
+points = rs.points()
 
 # Init the D435 pipeline capture
 pipeline = rs.pipeline()
@@ -44,26 +51,34 @@ while True:
     frame = np.asanyarray(frame_aligned.get_data())
     # get reference Depth state
     depth = np.asanyarray(depth_aligned.get_data())
-    print(depth.shape)
 
-    # Init point cloud in cartesian (2D)
-    pcc = np.zeros((640,2))
-    # Init point cloud in polar value
-    pcp = np.zeros((640,2))
+    # Tell pointcloud object to map to this color frame
+    pc.map_to(frame_aligned)
 
-    # 2D middle obstacle
-    for u in range(640):
-        z_val = depth[240-1, u]
-        x_val = (u-cx)*((z_val*depth_scale)/fx)
-        pcc[u, 1] = z_val
-        pcc[u, 0] = x_val
+    # Generate the pointcloud and texture mappings
+    points = pc.calculate(depth_aligned)
+    # return all vertex point without texture
+    vtx = np.asanyarray(points.get_vertices()).view(np.float32).reshape(-1, 3)
 
-    for u in range(640):
-        r_val = np.sqrt(pcc[u,0]**2 + pcc[u,1])
-        theta_val = np.degrees(np.arctan2(pcc[u,1], pcc[u,0]))
-        pcp[u,1] = r_val
-        pcp[u,0] = theta_val
+    # filter point between (Ymin, Ymax) and the min distance
+    Ymin = 0.28
+    Ymax = 2
+    Zmin = 0.30
+    filter_index = []
+    for i in range(vtx.shape[0]):
+        if vtx[i,1] < Ymin or vtx[i,1] > Ymax or vtx[i,2] < Zmin:
+            filter_index.append(i)
+    vtx = np.delete(vtx, filter_index, 0)
 
+    # Convert the cartesian point cloud to polar space
+    PSC = np.zeros((vtx.shape[0],2))
+    for j in range(vtx.shape[0]):
+        r = np.sqrt(vtx[j,0]**2 + vtx[j,2]**2)
+        theta = np.arctan2(vtx[j,2], vtx[j,0])
+        PSC[j,0] = theta
+        PSC[j,1] = r
+
+    # polar histogram + bubble OA 
 
     if cv2.waitKey(1) == 27:
         break
